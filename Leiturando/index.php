79968487caf519d8d49601c748bf0f45a58a2ede
Naelsod <Fael459@gmail.com>
2025-04-16 19:22:28 +0000

    Primeiro dia

---
+++ Leiturando/index.php
@@ -0,0 +1,25 @@
+<?php include('includes/header.php'); ?>
+<link rel="stylesheet" href="assets/css/style.css">
+
+<section class="sobre-nos">
+  <div class="container">
+    <div class="esquerda">
+      <img src="assets/img/livros1.png" alt="Livros empilhados com vaso">
+      <p>Fique ligado nas nossas redes sociais e junte-se à nossa comunidade:</p>
+      <div class="redes">
+        <a href="#"><img src="assets/img/instagram.svg" alt="Instagram"></a>
+        <a href="#"><img src="assets/img/tiktok.png" alt="TikTok"></a>
+      </div>
+    </div>
+    <div class="direita">
+      <h2><span>Sobre</span> Nós</h2>
+      <p>
+        O Leiturando é um projeto apaixonado pela leitura e pelo poder transformador dos livros...
+        <br><br>
+        Estamos empenhados em fazer a diferença, mostrando que a ficção pode se tornar realidade...
+      </p>
+    </div>
+  </div>
+</section>
+
+<?php include('includes/footer.php'); ?>
